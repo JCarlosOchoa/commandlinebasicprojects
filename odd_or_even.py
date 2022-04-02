b159cae6ac5c782797b684c7f29d0002d3ea693e
JCarlosOchoa <juanochoa123@gmail.com>
2022-04-01 21:03:06 -0500

    I learnedabout simpledialog module, which would be a more effective solution to my app. The app works for exactly one user input. The next step would be to get the app to allow user to input more than one number and allowing the user to decide when to stop.

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,21 +1,5 @@
 from tkinter import *
-
-def odd_or_even(num):
-    try:
-        int(num)
-    
-    except:
-        print("You didn't input an integer!")
-
-    else:
-        if (num % 2 == 0):
-
-            print ("even")
-        else:
-            print ("odd")
-
-def retrieve():
-    return num_entry.get()
+from tkinter import simpledialog
 
 #master
 root = Tk()
@@ -36,16 +20,48 @@ rightframe.pack(side=RIGHT)
 label = Label(frame, text = "What number are you thinking?")
 label.pack()
 
-#keyboard entry
-num_entry = Entry(frame, width = 20)
-num_entry.insert(0, "Number")
+#keyboard entry - didn't work because the initial value in num_entry will always be Number. Should try simpledialog
+# num_entry = Entry(frame, width = 20)
+# num_entry.insert(0, "Number")
+
+#simpledialog entry
+num_entry = simpledialog.askinteger(title="Choose a Number", prompt="Choose a Number", parent=root)
+
+#this label is where my message containing result will show up, placed within the master
+result = Label(root, text = "")
+result.pack(pady = 20)
+
+def odd_or_even(num):
+    
+    # I realized that I didn't need to add in an entirely new window
+    # global pop
+    # pop = Toplevel(root)
+    # pop.title("Result")
+    # pop.geometry("250x150")
+
+
+    try:
+        int(num)
+    
+    except:
+        # print("You didn't input an integer!")
+        result.config(text="You didn't input an integer!")
+
+    else:
+        if (num % 2 == 0):
+
+            result.config(text="Your number is even!")
+        else:
+            result.config(text="Your number is odd!")
+
+odd_or_even(num_entry)
 
 #how to center the entry?
-num_entry.pack(padx = 5, pady = 5)
+# num_entry.pack(padx = 5, pady = 5)
 
-#button that calls function on click
-Button = Button(frame, text = "Submit", command = odd_or_even(retrieve))
-Button.pack(padx = 5, pady = 5)
+#button that calls function on click, lambda passes inputs
+# button = Button(frame, text = "Submit", command = lambda: odd_or_even(num_entry.get()))
+# button.pack(padx = 5, pady = 5)
 
 root.title("Odd or Even?")
 root.mainloop()
